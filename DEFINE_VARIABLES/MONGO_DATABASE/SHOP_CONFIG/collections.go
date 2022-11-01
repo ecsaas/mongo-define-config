801f71e1c1f0877edbcc67c436e691eb0303b7af
manhbv.giaminh <manhbv.giaminh@gmail.com>
2022-11-01 14:59:37 +0700

    config shopPrivate

--- DEFINE_VARIABLES/MONGO_DATABASE/SHOP_CONFIG/collections.go
+++ DEFINE_VARIABLES/MONGO_DATABASE/SHOP_CONFIG/collections.go
@@ -2,6 +2,7 @@ package SHOP_CONFIG
 
 const ACCOUNT = "account"
 const SHOP = "shop"
+const SHOP_PRIVATE = "shopPrivate"
 const GOOGLE_USER = "googleUser"
 const ROLE = "role"
 const THEME = "theme"
